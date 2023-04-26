07358c65fddcb175f5927afca98706051d099026
Radu-Antonio <raduantonio_2525@yahoo.com>
2023-04-26 17:07:18 +0300

    modified my settings

--- backend/database.js
+++ backend/database.js
@@ -1,19 +1,18 @@
-import mysql from 'mysql2';
+import mysql from "mysql2";
 
-const db  = mysql.createConnection({
-    database: "mdsdb",
-    host: "localhost",
-    user: "root",
-    password: "mysql123"
-})
+const db = mysql.createConnection({
+  database: "mds", // aici
+  host: "localhost",
+  user: "root",
+  password: "alex123", // si aici modifica fiecare la baza lui de date
+});
 
 db.connect((err) => {
-    if(err){
-         console.log('Error in DB connection' + JSON.stringify(err,undefined,2) );
-    }
-    else{
-         console.log('DB Connected successfully' )
-    }
-})
+  if (err) {
+    console.log("Error in DB connection" + JSON.stringify(err, undefined, 2));
+  } else {
+    console.log("DB Connected successfully");
+  }
+});
 
-export default db
+export default db;
