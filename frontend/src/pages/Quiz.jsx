40fb611cab6edf6255451394c00773ba6b0903c3
Popovici Antonia-Adelina <popovici.antonia-adelina@s.unibuc.ro>
2023-06-13 00:40:20 +0300

    ttttt

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -1,76 +1,118 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import "../css/Quiz.css";
 
-const Quiz = (props) => {
-  const [questions, setQuestions] = useState([]);
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [selectedOption, setSelectedOption] = useState('');
-  const [timeLeft, setTimeLeft] = useState(60);
+const Quiz = () => {
+  // variables
+  const [questions, setQuestions] = useState([]); // array to store quiz questions
+  const [currentQuestion, setCurrentQuestion] = useState(0); // index of current question
+  const [selectedOption, setSelectedOption] = useState(""); // selected option for the current question
+  const [timeLeft, setTimeLeft] = useState(300); // countdown timer
+  const [correctAnswers, setCorrectAnswers] = useState(0); // count of correct answers
+  const [exerciseResults, setExerciseResults] = useState([]); // array to store exercise results
 
-  console.log(selectedOption);
+  // get quiz id
+  const { id } = useParams();
 
+  // fetch quiz data from the server
   useEffect(() => {
-    axios.get('http://localhost:3001/quiz/${params.match}')
-      .then(res => {
-        console.log("im here")
-        console.log('Response data:', res.data.questions);
+    axios
+      .get(`http://localhost:3001/quiz/${id}`)
+      .then((res) => {
+        console.log("Response data:", res.data.questions);
         setQuestions(res.data.questions);
       })
-      .catch(err => {
-        console.log('Error fetching quiz data:', err);
+      .catch((err) => {
+        console.log("Error fetching quiz data:", err);
       });
-  }, []);
+  }, [id]);
 
+  // timer
   useEffect(() => {
     let interval = null;
     if (timeLeft > 0) {
       interval = setInterval(() => {
-        setTimeLeft(timeLeft - 1);
+        setTimeLeft((prevTimeLeft) => prevTimeLeft - 1);
       }, 1000);
     } else {
       clearInterval(interval);
-      alert('Time is up!');
+      handleQuizCompletion(); // when time runs out, complete the quiz
     }
     return () => clearInterval(interval);
   }, [timeLeft]);
 
+  // handler for options
   const handleOptionChange = (e) => {
     setSelectedOption(e.target.value);
   };
 
-  const handleNextClick = () => {
-    setCurrentQuestion(currentQuestion + 1);
-    setSelectedOption('');
-  };
+  // handle quiz completion
+  const handleQuizCompletion = () => {
+    const currentQuestionData = questions[currentQuestion];
+    const isAnswerCorrect =
+      currentQuestionData && selectedOption === currentQuestionData.answer;
 
-  const handleSubmitClick = () => {
-    const answer = questions[currentQuestion].answer;
-    if (selectedOption === answer) {
-      alert('Correct answer!');
-    } else {
-      alert('Incorrect answer!');
+    setExerciseResults((prevExerciseResults) => [
+      ...prevExerciseResults,
+      { exerciseNumber: currentQuestion, isAnswerCorrect },
+    ]);
+
+    if (isAnswerCorrect) {
+      setCorrectAnswers((prevCorrectAnswers) => prevCorrectAnswers + 1);
     }
-    setCurrentQuestion(currentQuestion + 1);
-    setSelectedOption('');
+
+    setCurrentQuestion((prevCurrentQuestion) => prevCurrentQuestion + 1);
+    setSelectedOption("");
   };
 
+  // loading screen
   if (questions.length === 0) {
-    return (
-      <div>Loading...</div>
-    );
+    return <div>Loading...</div>;
   }
 
+  // check if time has expired
+  if (currentQuestion < questions.length && timeLeft === 0) {
+    handleQuizCompletion(); // complete the quiz when time expires
+  }
+
+  // results and correct answers
   if (currentQuestion >= questions.length) {
+    const totalQuestions = questions.length;
     return (
       <div>
         <h2>Quiz Complete</h2>
-        <p>You got X out of Y questions correct.</p>
+        <p>
+          You got {correctAnswers} out of {totalQuestions} questions correct.
+        </p>
+        {questions.map((question, index) => (
+          <div key={index}>
+            <p>
+              {index + 1}.{" "}
+              {exerciseResults.length > 0 &&
+              exerciseResults[index] &&
+              exerciseResults[index].isAnswerCorrect ? (
+                <span className="correct">Correct!</span>
+              ) : (
+                <span className="incorrect">
+                  Incorrect! Correct answer is "{question.answer}".
+                </span>
+              )}
+            </p>
+          </div>
+        ))}
       </div>
     );
   }
 
+  // render the current question
   const question = questions[currentQuestion];
-  const options = [question.option1, question.option2, question.option3, question.option4];
+  const options = [
+    question.option1,
+    question.option2,
+    question.option3,
+    question.option4,
+  ];
 
   return (
     <div>
@@ -80,15 +122,19 @@ const Quiz = (props) => {
         {options.map((option, index) => (
           <div key={index}>
             <label>
-              <input type="radio" value={option} checked={selectedOption === option} onChange={handleOptionChange} />
+              <input
+                type="radio"
+                value={option}
+                checked={selectedOption === option}
+                onChange={handleOptionChange}
+              />
               {option}
             </label>
           </div>
         ))}
       </div>
       <p>Time Left: {timeLeft} seconds</p>
-      <button onClick={handleNextClick}>Next</button>
-      <button onClick={handleSubmitClick}>Submit</button>
+      <button onClick={handleQuizCompletion}>Submit</button>
     </div>
   );
 };
--- frontend/src/pages/QuizList.jsx
+++ frontend/src/pages/QuizList.jsx
@@ -1,37 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import '../css/QuizList.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "../css/QuizList.css";
 
 const QuizList = () => {
   const [quizzes, setQuizzes] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/quizList')
-      .then(res => {
-        console.log('Response data:', res.data);
+    axios
+      .get("http://localhost:3001/quizList")
+      .then((res) => {
+        console.log("Response data:", res.data);
         setQuizzes(res.data);
       })
-      .catch(err => {
-        console.log('Error fetching quiz data:', err);
+      .catch((err) => {
+        console.log("Error fetching quiz data:", err);
       });
   }, []);
 
+  // description for the page
+  const pageDescription =
+    "Welcome to the Quiz List page! Here, you can explore a variety of quizzes and test your knowledge on different subjects. Choose a quiz from the list below and start challenging yourself. Have fun and enjoy the learning experience!";
+
   return (
     <div>
-    <div className="quiz-list">
-      <h2>List of Quizzes</h2>
-</div>
-    <div className="quiz-box">
-      {quizzes.map((quiz) => (
-        <div key={quiz.id} className="quiz-card">
-          <div className="quiz-title">{quiz.title}</div>
-          <div className="quiz-description">{quiz.description}</div>
-          <Link to={`/quiz/${quiz.id}`} className="quiz-link">Start Quiz</Link>
-        </div>
-      ))}</div>
+      <div className="quiz-list">
+        <h2 className="quiz-heading">List of Quizzes</h2>
+        <div className="quiz-description">{pageDescription}</div>
+      </div>
+      <div className="quiz-box">
+        {/* render each quiz card */}
+        {quizzes.map((quiz) => (
+          <div key={quiz.id} className="quiz-card">
+            <div className="quiz-title">{quiz.title}</div>
+            <div className="quiz-description">{quiz.description}</div>
+            {/* link to start the quiz */}
+            <Link to={`/quiz/${quiz.id}`} className="quiz-link">
+              Start Quiz
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default QuizList;
